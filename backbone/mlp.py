037189adffbe602153b1435cd64ebb7d6b94f9c4
Tao Zhuo <45383174+visiontao@users.noreply.github.com>
2023-06-11 11:00:22 +0800

    Add files via upload

--- backbone/mlp.py
+++ backbone/mlp.py
@@ -1,1 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class MLP(nn.Module):
+    def __init__(self, in_dim=28*28, out_dim=10):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(in_dim, 100)
+        self.fc2 = nn.Linear(100, 100)
+        self.classifier = nn.Linear(100, out_dim)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight, gain=1.0)  
+                
+    def get_params(self) -> torch.Tensor:
+        """
+        Returns all the parameters concatenated in a single tensor.
+        :return: parameters tensor (??)
+        """
+        params = []
+        for pp in list(self.parameters()):
+            params.append(pp.view(-1))
+        return torch.cat(params)
+    
+    def get_grads(self) -> torch.Tensor:
+        """
+        Returns all the gradients concatenated in a single tensor.
+        :return: gradients tensor (??)
+        """
+        return torch.cat(self.get_grads_list())
+
+    def get_grads_list(self):
+        """
+        Returns a list containing the gradients (a tensor for each layer).
+        :return: gradients list
+        """
+        grads = []
+        for pp in list(self.parameters()):
+            grads.append(pp.grad.view(-1))
+        return grads                    
+                
+
+    def forward(self, x):
+        b = x.size(0)
+        x = x.view(b, -1)
+                
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.classifier(x)
+        
+        return x    
+
+    
+class ConvNet(nn.Module):
+
+    def __init__(self, out_dim=10):
+
+        super(ConvNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
+        self.classifier = nn.Linear(500, out_dim)
+
+        self.encoder = nn.Sequential(
+            self.conv1,
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            self.conv2,
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Flatten(),
+            self.fc1,
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.classifier(x)
+        
+        return x
+    
